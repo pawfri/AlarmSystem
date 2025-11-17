4232f32b46cc0372b6cc01345caed21389285065
Patrick <pfriis@outlook.com>
2025-11-17 14:29:08 +0100

    Add SensorSmoke class

---
+++ Backend/AlarmSystemLib/SensorSmoke.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlarmSystemLib;
+
+public class SensorSmoke : SensorBase
+{
+    public bool IsSmokeDetected { get; set; }
+
+    public SensorSmoke(int id, string placement, SensorStatus status)
+        : base(id, placement, status)
+    {
+    }
+
+    public void SmokeDetected()
+    {
+        IsSmokeDetected = true;
+        Trigger();
+    }
+    public void SmokeClear()
+    {
+        IsSmokeDetected = false;
+        Restore();
+    }
+}
