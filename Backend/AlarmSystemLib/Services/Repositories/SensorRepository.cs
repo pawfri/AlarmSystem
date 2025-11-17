58a71cb7f90b9c836b03dc65f1f64bd9fbda4e01
Patrick <pfriis@outlook.com>
2025-11-17 15:26:56 +0100

    Add SensorRepository with CRUD operations fpr SensorBase

---
+++ Backend/AlarmSystemLib/Services/Repositories/SensorRepository.cs
@@ -0,0 +1,55 @@
+﻿using AlarmSystemLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlarmSystemLib.Services.Repositories;
+
+public  class SensorRepository
+{
+    private readonly List<SensorBase> _sensors = new();
+    private int _nextId = 1;
+
+    public List<SensorBase> GetAll()
+    {
+        return _sensors;
+    }
+
+    public SensorBase? GetById(int id)
+    {
+        return _sensors.FirstOrDefault(s => s.Id == id);
+    }
+
+    public SensorBase Add(SensorBase sensor)
+    {
+        sensor.Id = _nextId++;
+        _sensors.Add(sensor);
+        return sensor;
+    }
+
+    public SensorBase? Update(SensorBase sensor)
+    {
+        var existing = GetById(sensor.Id);
+        if (existing == null)
+        {
+            return null;
+        }
+
+        var index = _sensors.IndexOf(existing);
+        _sensors[index] = sensor;
+        return sensor;
+    }
+
+    public SensorBase? Delete(int id)
+    {
+        SensorBase sensor = GetById(id);
+        if (sensor != null)
+        {
+            _sensors.Remove(sensor);
+        }
+        return sensor;
+    }
+
+}
