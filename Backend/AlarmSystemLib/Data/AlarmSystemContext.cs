43a5bf5cfbd1aad272d34081468c164c25b4dc7b
Patrick <pfriis@outlook.com>
2025-11-20 21:15:44 +0100

    Add AlarmSystemContext

---
+++ Backend/AlarmSystemLib/Data/AlarmSystemContext.cs
@@ -0,0 +1,21 @@
+﻿using AlarmSystemLib.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlarmSystemLib.Data;
+
+public class AlarmSystemContext : DbContext
+{
+    public AlarmSystemContext(DbContextOptions<AlarmSystemContext> options) 
+        : base(options)
+    {
+    }
+
+    public DbSet<SensorMagnet> MangetSensors { get; set; }
+    public DbSet<SensorPIR> PIRSensors { get; set; }
+    public DbSet<SensorSmoke> SmokeSensors { get; set; }
+}
